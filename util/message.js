f072c9993e6d39b405a123829b2d963e0127e2c3
zhong <zhong>
2020-01-09 16:08:16 +0800

    sdad

--- util/message.js
+++ util/message.js
@@ -3,7 +3,7 @@ const xml2js = require('xml2js');
 
 function message(body, query) {
     console.log(1)
-    if (body.MsgType[0] === 'text') {
+    if (body.msgtype[0] === 'text') {
         console.log(2)
         let builder = new xml2js.Builder();
         console.log(3)
