17d0ba0f6772dd0d9bda47a0f2033d73cf9f4fc4
zhong <zhong>
2020-01-09 16:16:35 +0800

    是的撒

--- util/message.js
+++ util/message.js
@@ -2,11 +2,8 @@
 const xml2js = require('xml2js');
 
 function message(body, query) {
-    console.log(1)
     if (body.msgtype[0] === 'text') {
-        console.log(2)
         let builder = new xml2js.Builder();
-        console.log(3)
         let xml = {
             ToUserName: query.openid,
             FromUserName: body.fromusername,
